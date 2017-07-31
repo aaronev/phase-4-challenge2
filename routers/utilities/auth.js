e5eff3d734e7ed40cbfe2b01a37555cedc7ad00a
aaronev <aarone.villanueva@gmail.com>
2017-07-31 10:54:24 -0700

    adds authentication and authorized utilities for routers

---
+++ routers/utilities/auth.js
@@ -0,0 +1,55 @@
+const passport = require('../../config/authentication')
+const getAlbumsTable = require('../../domain/albums')
+const getReviewsTable = require('../../domain/reviews')
+const getUsersTable = require('../../domain/users')
+
+const authenticates = {}
+const deletes = {}
+const adds = {}
+
+authenticates.LocalStrategy = passport.authenticate('local', {
+  successRedirect: '/',
+  failureRedirect: '/sign-in',
+  failureFlash: true
+})
+
+adds.newUser = (req, res) => {
+  const {name, email, password} = req.body
+  ! req.user
+  ? res.redirect('/sign-in')
+  : getUsersTable.byEmail(email)
+  .then(foundEmail => {
+    if (foundEmail) {
+      req.flash('errorSignUp', 'Email already exist!')
+      res.redirect('/sign-up')
+    } else {
+      getUsersTable.toAdd(name, email, password, '/img/no-dj.png')
+      .then(addedUsers => { res.redirect('/sign-in') })
+    }
+  })
+}
+
+adds.newReview = (req, res, next) => {
+  getReviewsTable.toAdd(
+    req.user.id, 
+    req.params.id, 
+    req.body.review
+    ).then( reviews => {
+      res.redirect(`/albums/${req.params.id}`)
+  }).catch(next)
+} 
+
+deletes.review = (req, res, next) => {
+  ! req.user
+  ? res.redirect('/sign-in')
+  : getReviewsTable.toDelete(req.params.id)
+  .then( deletedReview => {
+    if(deletedReview) {
+    res.redirect(`/users/${req.user.id}`)
+    }
+  }).catch(next)
+}
+
+
+module.exports = {adds, deletes}
+module.exports = authenticates
